c362d3831152dc65d478bfe5fe916bb9d6ad22d5
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2024-09-02 22:47:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Education from "./components/Education";
-import Hero from "./components/Hero";
-import Navbar from "./components/navbar";
-import Projects from "./components/Projects";
-import Technologies from "./components/Technologies";
+import About from "/components/About";
+import Contact from "/components/Contact";
+import Education from "/components/Education";
+import Hero from "/components/Hero";
+import Navbar from "/components/navbar";
+import Projects from "/components/Projects";
+import Technologies from "/components/Technologies";
 
 const App = () => {
   return (
