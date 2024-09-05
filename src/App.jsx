3625b16de33f2b614df4f15b365ae7879aa156a3
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2024-09-06 00:16:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
 import Hero from "./components/Hero";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
