674d2c3217be27cc95d272826b0c4e3cb7b47d51
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2024-09-08 01:29:07 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -26,8 +26,7 @@ export const EDUCATION = [
     year: "2016 - 2018",
     role: " Dimploma in Computer Technology",
     company: " MSBTE",
-    description: 'I completed my Diploma in computer science in Bharati Vidyapeeth Institute if Technology kharghar navi mumbai.`,
-    technologies: ["HTML","CSS","Android Studio"],
+    description: 'I completed my Diploma in computer science in Bharati Vidyapeeth Institute if Technology kharghar navi mumbai.`,technologies: ["HTML","CSS","Android Studio"],
   },
   /*{
     year: "2020 - 2021",
