be0042722e922aea6126809539b7dc4437aa3b39
Sushant98K <sushant98k@gmail.com>
2024-09-09 23:50:45 +0530

    nothings

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,34 +1,38 @@
-import { CONTACT } from "../constants"
-import { motion } from "framer-motion"
+import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 
 const Contact = () => {
-  return ( 
-    <div className="border-b border-neutral-900 pb-4">
-        <motion.h2 
-          whileInView={{ opacity: 1, y: 0 }}
-          initial={{ opacity: 0, y: -100 }}
-          transition={{ duration: 0.5 }}
-          className="my-10 text-center text-4xl"
+  return (
+    <div className="pb-4 border-b border-neutral-900">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-4xl text-center"
+      >
+        Get in touch
+      </motion.h2>
+      <div className="tracking-tighter text-center">
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
         >
-            Get in touch
-        </motion.h2>
-        <div className="text-center tracking-tighter">
-            <motion.p 
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100}}
-              transition={{ duration: 1 }}
-              className="my-4">{CONTACT.address}
-            </motion.p>
-            <motion.p 
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100}}
-              transition={{ duration: 1 }}
-              className="my-4">{CONTACT.phoneNo}
-            </motion.p>
-            <a href="#">{CONTACT.email}</a>
-        </div>
+          {CONTACT.address}
+        </motion.p>
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.phoneNo}
+        </motion.p>
+        <a href="#">{CONTACT.email}</a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
