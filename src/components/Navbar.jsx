dc18b960f26eac4ab21cf3522c88e2b74a44ff43
Sushant98K <sushant98k@gmail.com>
2024-09-17 12:09:59 +0530

    raw

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,29 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaSquareInstagram } from "react-icons/fa6";
 
 const navbar = () => {
-  return <nav className="mb-20 flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center">
-      <a href="#"><img src={logo} alt="logo" /></a>
-    </div>
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <a href="https://www.linkedin.com/in/sushant98k/"><FaLinkedin /></a>
-      <a href="https://github.com/Sushant98K"><FaGithubSquare /></a>
-      <a href="#"><FaSquareXTwitter /></a>
-      <a href="#"><FaSquareInstagram /></a>      
+  return (
+    <nav className="flex items-center justify-between py-6 mb-20">
+      <div className="flex items-center flex-shrink-0">
+        <a href="#">
+          <img src={logo} alt="logo" />
+        </a>
+      </div>
+      <div className="flex items-center justify-center gap-4 m-8 text-2xl">
+        <a href="https://www.linkedin.com/in/sushant98k/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/Sushant98K">
+          <FaGithubSquare />
+        </a>
+        <a href="https://x.com/ig_ToyBreaker?s=09">
+          <FaSquareXTwitter />
+        </a>
+        <a href="https://www.instagram.com/ig_toybreaker?igsh=MTJqc2VmdnYzNDVzOA==">
+          <FaSquareInstagram />
+        </a>
+      </div>
+    </nav>
+  );
+};
 
-    </div>
-
-  </nav>
-}
-
-export default navbar
+export default navbar;
